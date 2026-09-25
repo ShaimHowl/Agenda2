25a4eaa2b8ace3c8ade720c5dd022e4110f38d81
ShaimHowl <showlader@elpuig.xeill.net>
2026-09-25 20:05:57 +0200

    Segunda parte hecha

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         System.out.print("Escoge una opcion ");
         int opcion = sc.nextInt();
 
-        System.out.println("Has escogido el numero: " + opcion);
+        System.out.println("Has escogido el numero " + opcion);
 
 
 
