97b882621c3a5a386e2334f79682582ee516d001
ShaimHowl <showlader@elpuig.xeill.net>
2026-09-25 19:54:00 +0200

    PRIMERA PARTE

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bienvenido a la agenda, por favor elija una opción:");
+        System.out.println("  1. Crear un nuevo contacto.");
+        System.out.println("  2. Buscar contactos existentes.");
+        System.out.println("  3. Actualizar un contacto existente.");
+        System.out.println("  4. Eliminar un contacto existente.");
+        System.out.println("  5. Salir.");
+
+
+
+
+
+
+
+
 
     }
 }
